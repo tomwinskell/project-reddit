2c453bdf40823cab8abc6bdc5b67b633e5b42f70
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-02-03 12:52:07 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const main = document.querySelector('.main');
+
+const obj = main.addEventListener(('click', (event) => {
+  if (event.target.)
+}))
+
+function createPostElement(obj) {
+  const element = document.createElement('div').setAttribute()
+  return element.appendChild(document.createTextNode(`${obj.text} - Poster by: ${obj.name}`));
+}
+
+function appendToMain(domElement, htmlElement) {
+  return domElement.appendChild(htmlElement);
+}
