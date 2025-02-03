22e89800f90ac75b63470128f705d6a3010ad0ed
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-02-03 14:40:43 -0500

    User can add posts.

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 const main = document.querySelector('.main');
+const posts = document.querySelector('.posts');
+const postForm = document.querySelector('.post-form');
 
-const obj = main.addEventListener(('click', (event) => {
-  if (event.target.)
-}))
+main.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains('add-post')) {
+    appendToMain(posts, createPostElement(getPostInput()));
+    clearInput();
+  }
+})
+
+function getPostInput() {
+  return Array.from(postForm.querySelectorAll('input')).reduce((struct, current) => {
+    struct[current.name] = current.value;
+    return struct;
+  }, {})
+}
+
+function clearInput() {
+  postForm.querySelectorAll('input').forEach(input => {
+    input.value = '';
+  })
+}
 
 function createPostElement(obj) {
-  const element = document.createElement('div').setAttribute()
-  return element.appendChild(document.createTextNode(`${obj.text} - Poster by: ${obj.name}`));
+  const el = document.createElement('div');
+  el.classList.add('py-3')
+  el.appendChild(document.createTextNode(`${obj.text} - Posted by: ${obj.name}`));
+  return el;
 }
 
 function appendToMain(domElement, htmlElement) {
   return domElement.appendChild(htmlElement);
 }
+
+
